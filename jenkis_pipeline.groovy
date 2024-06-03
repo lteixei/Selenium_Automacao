61ea3346d2eceea134290318fad5394ad75baa62
lteixei <53909935+lteixei@users.noreply.github.com>
2024-06-03 13:34:22 +0100

    Update jenkis_pipeline.groovy

--- jenkis_pipeline.groovy
+++ jenkis_pipeline.groovy
@@ -6,19 +6,28 @@ pipeline {
             steps {
                 git branch: 'main', url: 'https://github.com/lteixei/Selenium_Automacao.git'
                 echo 'Hello World'
+                stage('Build code')
+                {
+                    steps
+                    {
+                        bat script: 'mvn compile'
+                    }
+                }
+                stage('Run Test')
+                {
+                    steps
+                    {
+                        bat script: 'mvn test - Dbrowser=localchrome'
+                    }
+                }
+                stage('Publish Report')
+                {
+                    steps
+                    {
+                        publishHTML([allowMissing: false, alwaysLinkToLastBuild: true, keepAll: true, reportDir: '', reportFiles: 'target/surefire-reports/Extent*.html', reportName: 'Pipeline', reportTitles: '', useWrapperFileDirectly: true])
+                    }
+                }
             }
         }
-        stage('Build code')
-        {
-            bat script: 'mvn compile'
-        }
-        stage('Run Test')
-        {
-            bat script: 'mvn test - Dbrowser=localchrome'
-        }
-        stage('Publish Report')
-        {
-            publishHTML([allowMissing: false, alwaysLinkToLastBuild: true, keepAll: true, reportDir: '', reportFiles: 'target/surefire-reports/Extent*.html', reportName: 'Pipeline', reportTitles: '', useWrapperFileDirectly: true])
-        }
     }
-}
+}    
